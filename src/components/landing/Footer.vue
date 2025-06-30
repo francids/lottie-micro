9be4e7931464530f170378ab35c658d12a03783c
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-06-30 14:56:02 -0400

    Add PrimeVue Volt Library

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -3,7 +3,7 @@ import AppLogo from "../common/AppLogo.vue";
 </script>
 
 <template>
-  <footer class="bg-neutral-50 border-t border-neutral-200 mt-20">
+  <footer class="bg-surface-50 border-t border-surface-200 mt-20">
     <div class="max-w-7xl mx-auto px-6 py-12">
       <div
         class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-6 md:space-y-0"
@@ -11,14 +11,14 @@ import AppLogo from "../common/AppLogo.vue";
         <!-- Logo and Description -->
         <div class="space-y-4">
           <AppLogo class="h-6 w-auto" />
-          <p class="text-neutral-600 text-sm max-w-md">
+          <p class="text-surface-600 text-sm max-w-md">
             A simple lottie editor for creating animations efficiently.
           </p>
         </div>
 
         <!-- Copyright -->
         <div>
-          <p class="text-sm text-neutral-500">
+          <p class="text-sm text-surface-500">
             © {{ new Date().getFullYear() }} Lottie Micro. All rights reserved.
           </p>
         </div>
--- src/components/landing/HeroSection.vue
+++ src/components/landing/HeroSection.vue
@@ -2,14 +2,14 @@
   <main class="min-h-screen max-w-7xl mx-auto px-6 py-20">
     <div class="text-center">
       <h1
-        class="text-3xl md:text-4xl lg:text-5xl font-bold text-neutral-900 mb-4"
+        class="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 text-surface-900"
       >
         A Minimal Lottie Editor
         <br />
-        <span class="text-sky-500">for developers</span>
+        <span class="text-primary">for developers</span>
       </h1>
 
-      <p class="text-lg text-neutral-600 mb-12 max-w-2xl mx-auto">
+      <p class="text-lg mb-12 max-w-2xl mx-auto text-surface-600">
         Create and edit lottie animations with a simple, lightweight editor.
       </p>
     </div>
